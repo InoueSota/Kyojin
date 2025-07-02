ab06739e9b7e45550f6eef7263c67393c3d76f95
InoueSota <k022g0045@g.neec.ac.jp>
2025-07-02 13:38:31 +0900

    ゲームループ作成途中

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,28 @@ public class GameManager : MonoBehaviour
     // My Component
     private InputManager inputManager;
 
+    [Header("Game Parameter")]
+    [SerializeField] private float maxTime;
+    private float remainingTime;
+    private int discoveryCount;
+
     void Start()
     {
         // Set Component
         inputManager = GetComponent<InputManager>();
+
+        // Set Parameter
+        remainingTime = maxTime;
     }
 
     void Update()
     {
         // “ü—Íó‹µ‚ğæ“¾‚·‚é
         inputManager.GetAllInput();
+    }
+    void RemainingTime()
+    {
+        remainingTime -= Time.deltaTime;
     }
 
     void LateUpdate()
